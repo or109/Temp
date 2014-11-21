abbfd77aba022f2648d81a513cd737ebf8be9e10
OrMan <orm109@gmail.com>
2014-11-21 21:42:56 +0200

    my new class

---
+++ MyTest/src/add/Try.java
@@ -0,0 +1,19 @@
+package add;
+
+public class Try {
+	public Try() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
