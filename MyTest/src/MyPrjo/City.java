72d6311bfd797ac9ddbf9c3be9256633f11fcae4
OrMan <orm109@gmail.com>
2014-11-21 21:29:52 +0200

    fdfs

--- MyTest/src/MyPrjo/City.java
+++ MyTest/src/MyPrjo/City.java
@@ -2,9 +2,13 @@ package MyPrjo;
 
 public class City {
 	int yoyo;
+
 	public City() {
 		// TODO Auto-generated constructor stub
 		this.yoyo = 1;
+		System.out.println("Matser Best");
+		System.out.println("Matser Best");
+
 		System.out.println("Matser Best");
 	}
 }
