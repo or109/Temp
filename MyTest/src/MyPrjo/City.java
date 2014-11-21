360b238c5eda063690a0c68723035d6be2b621c7
OrMan <orm109@gmail.com>
2014-11-21 21:51:00 +0200

    YYRAR

--- MyTest/src/MyPrjo/City.java
+++ MyTest/src/MyPrjo/City.java
@@ -12,5 +12,7 @@ public class City {
 		System.out.println("Matser Best");
 		
 		System.out.println("THa mada faka NEW BRACNH");
+		System.out.println("THa mada faka NEW BRACNH");
+		System.out.println("THa mada faka NEW BRACNH");
 	}
 }
