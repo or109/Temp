980e01f6039059b4a2247cf61217e7c42bc5fccd
OrMan <orm109@gmail.com>
2014-11-21 20:43:01 +0200

    fsdfsff

--- MyTest/src/Lolo.java
+++ MyTest/src/Lolo.java
@@ -2,5 +2,11 @@ public class Lolo {
 	public Lolo() {
 		// TODO Auto-generated constructor stub
 		System.out.println("my ! ");
+		System.out.println("my ! ");
+		System.out.println("my ! ");
+		System.out.println("my ! ");
+		System.out.println("my ! ");
+		System.out.println("my ! ");
+		System.out.println("my ! ");
 	}
 }
